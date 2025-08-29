781a60aba5c44d28f4b45951e34b299d975939d5
Sophia Hoffmann Fernandes Braga <hero.b.braga@gmail.com>
2025-08-29 16:56:08 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ echo scti | sudo -S rm -rf .nix-profile/bin/dockerd-rootless
 echo scti | sudo -S apt-get update
 echo scti | sudo -S apt install -y python3-pip 
 pip install pyspark --break-system-packages 
-echo scti | sudo -S snap docker 
+echo scti | sudo -S snap install docker 
 echo my-app | npx create-expo-app@latest --template default 
 cd my-app 
 echo "^C" | npm start 
