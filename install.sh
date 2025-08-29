a8b167a2c27f269606d85edcab94e2dbbb2f0fe1
Sophia Hoffmann Fernandes Braga <hero.b.braga@gmail.com>
2025-08-29 16:36:16 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 cd 
 echo scti | sudo -S rm -rf .nix-profile/bin/android-studio 
+echo scti | sudo -S apt-get update
 echo scti | sudo -S apt install -y python3-pip 
 pip install pyspark --break-system-packages 
 echo scti | sudo -S apt install -y docker 
